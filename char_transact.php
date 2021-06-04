2c8498433efa3248eeb8a474edf3bdd3bbe5d409
bpavlovsoft-dev <85349452+bpavlovsoft-dev@users.noreply.github.com>
2021-06-04 16:27:50 +0300

    Add files via upload

--- char_transact.php
+++ char_transact.php
@@ -10,7 +10,14 @@
       $alias = mysqli_real_escape_string($db, $_POST['alias']);
       $real_name = mysqli_real_escape_string($db, $_POST['real_name']);
       $address = mysqli_real_escape_string($db, $_POST['address']);
-      
+      $city = mysqli_real_escape_string($db, $_POST['city']);
+      $state = mysqli_real_escape_string($db, $_POST['state']);
+      $zipcode_id = mysqli_real_escape_string($db, $_POST['zipcode_id']);
+      $alingment = mysqli_real_escape_string($db, $_POST['alingment']);
+      $query = 'INSERT IGNORE INTO comic_zipcode (zipcode_id, city, state)
+        VALUES
+          ("' . $zipcode_id . '", "' . $city . '", "' . $state . '")';
+      mysqli_query($db, $query) or die(mysqli_error($db));    
     break;
-  }    
+  }
 ?>
